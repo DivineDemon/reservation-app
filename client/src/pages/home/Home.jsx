b27ad2cb20a5825865c43a85a89668576062bb18
DivineDemon <supame123@gmail.com>
2022-05-31 12:37:00 +0400

    Properties Done! ✨

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "./../../components/header/Header";
 import Featured from "./../../components/featured/Featured";
+import PropertyList from "./../../components/propertyList/PropertyList";
+import FeaturedProperties from "./../../components/featuredProperties/FeaturedProperties";
 
 const Home = () => {
   return (
@@ -13,6 +15,9 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <div className="homeTitle">Home guests love</div>
+        <FeaturedProperties />
       </div>
     </div>
   );
