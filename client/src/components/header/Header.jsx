398957a5eefe25930fff5775df9903b277e801dc
DivineDemon <supame123@gmail.com>
2022-05-31 08:40:41 +0400

    Added Fontawesome! ✨

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="headerList">
+        <div className="headerListItem"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
