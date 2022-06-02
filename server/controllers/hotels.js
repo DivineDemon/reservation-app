9312cd28f9bbec4bf81fa3b2c79384c30a18a359
DivineDemon <supame123@gmail.com>
2022-06-02 14:19:39 +0400

    Created Hotel! ✨

--- server/controllers/hotels.js
+++ server/controllers/hotels.js
@@ -1,3 +1,17 @@
-export const createHotel = (req, res) => {
-  res.send("Create Hotel");
+import Hotel from "../models/Hotel.js";
+
+export const createHotel = async (req, res) => {
+  try {
+    const savedHotel = await Hotel.create(req.body);
+    res.status(201).json({
+      status: true,
+      message: "Successfully Created Hotel!",
+      data: savedHotel,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      error: error.message,
+    });
+  }
 };
