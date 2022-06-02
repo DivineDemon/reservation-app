9db41c0adb54ec2759d54edf57c1ae89430fcecb
DivineDemon <supame123@gmail.com>
2022-06-02 12:55:24 +0400

    Server Boilerplate! :sparkles:

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import cors from "cors";
+import dotenv from "dotenv";
+
+import express from "express";
+import path from "path";
+
+// Configured .env
+dotenv.config({ path: "../.env" });
+
+// Importing Routes
+
+// Initializing Express App
+const app = express();
+
+// Middleware
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "Base URL Working!" });
+});
+
+// Starting the App
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server Running on Port: ${PORT}`);
+});
