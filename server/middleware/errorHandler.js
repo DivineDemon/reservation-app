9a426f3303110d53ea176b37b25bee748a495c26
DivineDemon <supame123@gmail.com>
2022-06-05 09:46:49 +0400

    :hammer:

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -3,6 +3,8 @@ export const errorHandler = (err, req, res, next) => {
   const errMessage = err.message || "Something went Wrong!";
   res.status(errStatus);
   res.json({
+    success: false,
+    status: errStatus,
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
