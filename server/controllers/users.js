21db549b1c16f5fb0d7f812cc66a2eb9245f8c01
DivineDemon <supame123@gmail.com>
2022-06-05 13:35:59 +0400

    User CRUD Done! ✨

---
+++ server/controllers/users.js
@@ -0,0 +1,58 @@
+import User from "../models/User.js";
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: true,
+      message: "Successfully Fetched Users!",
+      data: users,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      status: true,
+      message: "Successfully Fetched User!",
+      data: user,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(201).json({
+      status: true,
+      message: "Successfully Updated User!",
+      data: updatedUser,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(201).json({
+      status: true,
+      message: "Successfully Deleted User!",
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,12 @@
 import express from "express";
+import {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/users.js";
 
 export const router = express.Router();
 
-router.get("/register", (req, res) => {
-  res.send("Register");
-});
+router.route("/").get(getUsers);
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
