70ecc67c34e2509ffbe99ba017cdfe495fb03692
DivineDemon <supame123@gmail.com>
2022-06-06 14:20:25 +0400

    Modifying getHotels to provide searchFilters! ✨

--- server/controllers/hotels.js
+++ server/controllers/hotels.js
@@ -14,8 +14,12 @@ export const createHotel = async (req, res, next) => {
 };
 
 export const getHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min || 1, $lt: max || 1000 },
+    }).limit(req.query.limit);
     res.status(200).json({
       status: true,
       message: "Successfully Fetched Hotels!",
