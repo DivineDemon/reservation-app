bc05c03b164017509c68b6729c5a2548bf06c61d
DivineDemon <supame123@gmail.com>
2022-06-07 14:04:08 +0400

    User Session Implementation! ✨

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { DateRange } from "react-date-range";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
@@ -17,6 +17,7 @@ import "./header.css";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { SearchContext } from "./../../context/SearchContext";
+import { AuthContext } from "./../../context/AuthContext";
 
 const Header = ({ type }) => {
   const [destination, setDestination] = useState("");
@@ -36,6 +37,7 @@ const Header = ({ type }) => {
   });
 
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
@@ -91,7 +93,11 @@ const Header = ({ type }) => {
               Get rewarded for your travels - unlock instant savings of 10% or
               more with a free HotelBooking account
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            {user ? (
+              <span>Let's Plan an Excellent Getaway, {user.username}!</span>
+            ) : (
+              <button className="headerBtn">Sign in / Register</button>
+            )}
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
