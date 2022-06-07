61ce9c90918af2c0a512e95355b454a2e70c1757
DivineDemon <supame123@gmail.com>
2022-06-07 13:56:30 +0400

    Logout Done! ✨

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,37 @@
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { AuthContext } from "./../../context/AuthContext";
 import "./navbar.css";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { user, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/login");
+  };
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">HotelBooking</span>
         </Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? (
+          <div className="navItems">
+            <span>Welcome, {user.username}!</span>
+            <button onClick={handleClick} className="navButton">
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
       </div>
     </div>
   );
