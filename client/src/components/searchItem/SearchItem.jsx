c256c34015ec7960bb603a8d4b2a20ad08fc3a6a
DivineDemon <supame123@gmail.com>
2022-06-07 10:02:11 +0400

    Working on Filtered Search! ✨

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -1,17 +1,14 @@
 import "./searchItem.css";
+import { Link } from "react-router-dom";
 
-const SearchItem = () => {
+const SearchItem = ({ item }) => {
   return (
     <div className="searchItem">
-      <img
-        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
-        alt=""
-        className="siImg"
-      />
+      <img src={item.photos[0]} alt="" className="siImg" />
       <div className="siDesc">
-        <h1 className="siTitle">Tower Street Apartments</h1>
-        <span className="siDistance">500m from center</span>
-        <span className="siTaxiOp">Free airport taxi</span>
+        <h1 className="siTitle">{item.name}</h1>
+        <span className="siDistance">{item.distance}</span>
+        <span className="siTaxiOp">{item.desc}</span>
         <span className="siSubtitle">
           Studio Apartment with Air conditioning
         </span>
@@ -24,14 +21,35 @@ const SearchItem = () => {
         </span>
       </div>
       <div className="siDetails">
-        <div className="siRating">
-          <span>Excellent</span>
-          <button>8.9</button>
-        </div>
+        {item.rating && (
+          <div className="siRating">
+            <span>
+              {(() => {
+                switch (item.rating) {
+                  case 1:
+                    return "1 Star";
+                  case 2:
+                    return "2 Stars";
+                  case 3:
+                    return "3 Stars";
+                  case 4:
+                    return "4 Stars";
+                  case 5:
+                    return "5 Stars";
+                  default:
+                    return "Unrated";
+                }
+              })()}
+            </span>
+            <button>{item.rating}</button>
+          </div>
+        )}
         <div className="siDetailTexts">
-          <span className="siPrice">$112</span>
+          <span className="siPrice">${item.cheapestPrice}</span>
           <span className="siTaxOp">Includes taxes and fees</span>
-          <button className="siCheckButton">See availability</button>
+          <Link to={`/hotels/${item._id}`}>
+            <button className="siCheckButton">See availability</button>
+          </Link>
         </div>
       </div>
     </div>
