b0a2001a609f015c005b1cbaa67291bbd7ce6b06
DivineDemon <supame123@gmail.com>
2022-06-14 09:26:58 +0400

    Added Protected Route!✨

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,7 +3,7 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
@@ -14,15 +14,27 @@ function App() {
   const { darkMode } = useContext(DarkModeContext);
   const ProtectedRoute = ({ children }) => {
     const { user } = useContext(AuthContext);
+    if (!user) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
   };
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
+          <Route path="login" element={<Login />} />
           <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
