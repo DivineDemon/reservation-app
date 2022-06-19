4e951f85f1d3b5f2739fa2b66b9f9ed0d4aab921
DivineDemon <supame123@gmail.com>
2022-06-19 08:16:51 +0400

    Completed! :rocket: ✨

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -12,38 +12,12 @@ export const createRoom = async (req, res, next) => {
       await Hotel.findByIdAndUpdate(hotelId, {
         $push: { rooms: savedRoom._id },
       });
-    } catch (error) {
-      next(error);
+    } catch (err) {
+      next(err);
     }
-    res.status(201).json({
-      status: true,
-      message: "Successfully Created Room!",
-      data: savedRoom,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const getRooms = async (req, res, next) => {
-  try {
-    const rooms = await Room.find();
-    res.status(200).json(rooms);
-  } catch (error) {
-    return next(error);
-  }
-};
-
-export const getRoom = async (req, res, next) => {
-  try {
-    const room = await Room.findById(req.params.id);
-    res.status(200).json({
-      status: true,
-      message: "Successfully Fetched Room!",
-      data: room,
-    });
-  } catch (error) {
-    return next(error);
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -51,18 +25,12 @@ export const updateRoom = async (req, res, next) => {
   try {
     const updatedRoom = await Room.findByIdAndUpdate(
       req.params.id,
-      {
-        $set: req.body,
-      },
+      { $set: req.body },
       { new: true }
     );
-    res.status(201).json({
-      status: true,
-      message: "Successfully Updated Room!",
-      data: updatedRoom,
-    });
-  } catch (error) {
-    return next(error);
+    res.status(200).json(updatedRoom);
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -76,12 +44,9 @@ export const updateRoomAvailability = async (req, res, next) => {
         },
       }
     );
-    res.status(201).json({
-      status: true,
-      message: "Successfully Updated Room Status!",
-    });
-  } catch (error) {
-    return next(error);
+    res.status(200).json("Room Status has been Updated!");
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -93,14 +58,29 @@ export const deleteRoom = async (req, res, next) => {
       await Hotel.findByIdAndUpdate(hotelId, {
         $pull: { rooms: req.params.id },
       });
-    } catch (error) {
-      next(error);
+    } catch (err) {
+      next(err);
     }
-    res.status(201).json({
-      status: true,
-      message: "Successfully Deleted Room!",
-    });
-  } catch (error) {
-    next(error);
+    res.status(200).json("Room Deleted!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err);
   }
 };
